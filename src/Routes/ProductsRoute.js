465a78a05931e3b40b9685df028ac2e9ada44c34
Pablo <cv.juanp@gmail.com>
2023-02-01 11:21:22 -0700

    ADD route to add new products

--- src/Routes/ProductsRoute.js
+++ src/Routes/ProductsRoute.js
@@ -12,5 +12,22 @@ ProductsRoute.get("/", async (req, res) => {
     res.send(e.message);
   }
 });
+//Create new product and verify that new product does not exist in database
+ProductsRoute.post("/", async (req, res) => {
+  try {
+    const findDuplicate = await ProductsModel.where("name").equals(
+      req.body.name
+    );
+    if (findDuplicate.length !== 0) {
+      res.send({ msg: "Name is already in database" });
+      return;
+    }
+    const newProduct = new ProductsModel({ name: req.body.name });
+    await newProduct.save();
+    res.send(newProduct);
+  } catch (e) {
+    res.send(e.message);
+  }
+});
 
 module.exports = ProductsRoute;
