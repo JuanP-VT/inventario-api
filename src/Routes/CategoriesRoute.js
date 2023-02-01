304c0184bfe7bd5db9902fa3e79e9ee5c2e6ed02
Pablo <cv.juanp@gmail.com>
2023-02-01 10:46:19 -0700

    ADD route to add new categories to database

--- src/Routes/CategoriesRoute.js
+++ src/Routes/CategoriesRoute.js
@@ -11,5 +11,24 @@ CategoriesRoute.get("/", async (req, res) => {
     res.send(e.message);
   }
 });
-
+//Create new categorie and verify that new categorie does not exist in database
+CategoriesRoute.post("/", async (req, res) => {
+  try {
+    const findDuplicate = await CategoriesModel.where("name").equals(
+      req.body.name
+    );
+    if (findDuplicate.length !== 0) {
+      res.send({ msg: "Name is already in database" });
+      return;
+    }
+    const newCategorie = new CategoriesModel({
+      name: req.body.name,
+      iconUrl: req.body.iconUrl,
+    });
+    await newCategorie.save();
+    res.send({ msg: "Succes" });
+  } catch (e) {
+    res.send({ msg: e.message });
+  }
+});
 module.exports = CategoriesRoute;
