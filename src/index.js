7880105389d1931d163ad95db049a1240888e9d2
Pablo <cv.juanp@gmail.com>
2023-02-01 09:08:09 -0700

    ADD Categories DB Model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-const express = require("express")
-const mongoose = require("mongoose")
-require('dotenv').config()
-const app = express()
-mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGOURL)
-app.listen(process.env.PORT)
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//Database Models
+const Categories = require("./Models/CategoriesModel");
+
+const app = express();
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGOURL);
+app.listen(process.env.PORT);
